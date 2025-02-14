4f8749380bc1724dbcfdcea5a1797d06416f140c
nycxcristie <104367483+nycxcristie@users.noreply.github.com>
2025-02-14 17:29:16 -0500

    updated js- shows city input and updated temperature shows degF

--- src/script.js
+++ src/script.js
@@ -1,13 +1,24 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let realCelsiusTemperature = response.data.temperature.current;
+  let realFTemperature = (realCelsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(realFTemperature);
+}
+
+function searchCity(city) {
+  // make api call and update the interface
+  let key = "f3e16t09873bb0ba34afa0c464d14o8b";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${key}`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSearch(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#newCity");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInputElement.value;
+  searchCity(searchInputElement.value);
 }
 
 let searchFormElement = document.querySelector("#searchForm");
 searchFormElement.addEventListener("submit", handleSearch);
-
-let key = "f3e16t09873bb0ba34afa0c464d14o8b";
-let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?query=chantilly&key=${key}&units=metric";
