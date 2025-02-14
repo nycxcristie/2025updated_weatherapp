d6acdb458a342c57fabed04a3728a7818b193826
nycxcristie <104367483+nycxcristie@users.noreply.github.com>
2025-02-14 16:24:14 -0500

    added axios into index.html and included key and apiurl in the js

--- src/script.js
+++ src/script.js
@@ -7,3 +7,7 @@ function handleSearch(event) {
 
 let searchFormElement = document.querySelector("#searchForm");
 searchFormElement.addEventListener("submit", handleSearch);
+
+let key = "f3e16t09873bb0ba34afa0c464d14o8b";
+let apiUrl =
+  "https://api.shecodes.io/weather/v1/current?query=chantilly&key=${key}&units=metric";
